ab267a7058dd1a8b69b0fec15a0da68302e0f0fb
Sam <samgauck@icloud.com>
2018-08-22 17:47:58 -0400

    Add multi-OS random function and Rollable protocol

--- Sources/DiceKit/DiceKit.swift
+++ Sources/DiceKit/DiceKit.swift
@@ -1,3 +1,23 @@
-struct DiceKit {
-    var text = "Hello, World!"
+#if !os(Linux)
+import Foundation
+#endif
+internal class Random {
+    #if os(Linux)
+    static var initialized = false
+    #endif
+    static func random(min: Int = 0, max: Int) -> Int {
+        #if os(Linux)
+        if !initialized {
+            srandom(UInt32(time(nil)))
+            initialized = true
+        }
+        return (random() % (max - min)) + min
+        #else
+        return Int(arc4random_uniform(UInt32(max - min))) + min
+        #endif
+    }
+}
+
+public protocol Rollable {
+    func roll() -> Roll
 }
