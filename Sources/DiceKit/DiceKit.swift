18c6759bf547c965c637c3ef12d5328a573b5812
Sam <samgauck@icloud.com>
2018-08-23 10:07:08 -0400

    Update & document random function

--- Sources/DiceKit/DiceKit.swift
+++ Sources/DiceKit/DiceKit.swift
@@ -1,9 +1,13 @@
 import Foundation
 
-internal class Random {
-    static func random(min: Int = 0, max: Int) -> Int {
-        return Int(arc4random_uniform(UInt32(max - min))) + min
-    }
+/// A function that returns a random number from `min` (inclusive) to `max` (exclusive).
+///
+/// - Parameters:
+///   - min: The minimum Int that can be returned (inclusive).
+///   - max: The maximum Int that can be returned (exclusive).
+/// - Returns: A random Int in the range [min, max).
+internal func random(min: Int = 0, max: Int) -> Int {
+    return Int(arc4random_uniform(UInt32(max - min))) + min
 }
 
 public protocol Rollable {
