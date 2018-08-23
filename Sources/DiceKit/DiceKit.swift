9ef9988982c636f68ebd66703339829dd6181986
Sam <samgauck@icloud.com>
2018-08-23 07:46:37 -0400

    Remove Linux conditional compilation statements

--- Sources/DiceKit/DiceKit.swift
+++ Sources/DiceKit/DiceKit.swift
@@ -1,20 +1,8 @@
-#if !os(Linux)
 import Foundation
-#endif
+
 internal class Random {
-    #if os(Linux)
-    static var initialized = false
-    #endif
     static func random(min: Int = 0, max: Int) -> Int {
-        #if os(Linux)
-        if !initialized {
-            srandom(UInt32(time(nil)))
-            initialized = true
-        }
-        return (random() % (max - min)) + min
-        #else
         return Int(arc4random_uniform(UInt32(max - min))) + min
-        #endif
     }
 }
 
