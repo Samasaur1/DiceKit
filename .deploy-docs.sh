cdbfea08a6cabf5b7754859c1344c8f82609d396
Sam <30577766+Samasaur1@users.noreply.github.com>
2019-08-02 10:30:29 -0400

    Make sure the entire image is visible

--- .deploy-docs.sh
+++ .deploy-docs.sh
@@ -7,7 +7,7 @@ rm -rf DiceKit.xcodeproj
 swift package generate-xcodeproj
 jazzy
 cd docs
-echo 'section > section > p > img { margin-top: 3em; }' >> css/jazzy.css
+echo 'section > section > p > img { margin-top: 4em; margin-right: 2em; }' >> css/jazzy.css
 git config --global user.name "Documentation Bot"
 git config --global user.email "docbot@travis-ci.com"
 git init
