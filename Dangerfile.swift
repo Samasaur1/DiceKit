57566a2b7f569b11c16a9df00ff73927087b1a24
Sam <samgauck@icloud.com>
2020-10-17 18:14:56 -0400

    Update Dangerfile with comments and to perform more tests

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -3,7 +3,9 @@ import Foundation
 let danger = Danger()
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 
+// MARK: - PR to master
 if danger.github.pullRequest.base.ref == "master" {
+    // MARK: New version (PR title)
     let newVersion: String
     if let range = danger.github.pullRequest.title.range(of: #"(?<=Version )\d+\.\d+\.\d+(?=: .+)"#, options: .regularExpression) {
         newVersion = String(danger.github.pullRequest.title[range])
@@ -12,11 +14,36 @@ if danger.github.pullRequest.base.ref == "master" {
         fail("There is no version in the pull request title!")
         newVersion = "0.0.0"
     }
+    //MARK: New version (.jazzy.yaml)
+    let jazzyVersion = String(danger.utils.readFile(".jazzy.yaml").split(separator: "\n")[4].dropFirst(17))
+    let jazzyVersionUpdated: Bool
+    if editedFiles.contains(".jazzy.yaml") {
+        jazzyVersionUpdated = true
+    } else {
+        fail("Version was not updated in .jazzy.yaml!")
+        fail(message: "Version was not updated in .jazzy.yaml!", file: ".jazzy.yaml", line: 5)
+        jazzyVersionUpdated = false
+    }
+    switch (newVersion, jazzyVersionUpdated) {
+    case ("0.0.0", false):
+        fail("No new version was indicated!")
+    case ("0.0.0", true):
+        message("PR title should be amended to \"Version \(jazzyVersion): \(danger.github.pullRequest.title)\"")
+    case (_, false):
+        suggestion(code: "module_version: \(newVersion)", file: ".jazzy.yaml", line: 5)
+    case (_, true):
+        message("Version was updated in both locations!")
+    }
 
     if danger.github.pullRequest.title.contains("$DESCRIPTION") {
         fail("$DESCRIPTION placeholder has not been filled in!")
     }
 
+    if danger.github.pullRequest.body?.split(separator: "\n").suffix(3).map(String.init) != ["1. Wait for CI", "1. Merge", "1. Run `release.sh`"] {
+        warn("Remember to run `release.sh` after merging!")
+    }
+
+    //MARK: Changelog edited
     containsChangelog: if editedFiles.contains("CHANGELOG.md") {
         let lineNumber = danger.utils.lines(for: "## ", inFile: "CHANGELOG.md")[1]
         let line = danger.utils.readFile("CHANGELOG.md").split(separator: "\n").filter { $0.hasPrefix("## ") }[1]
@@ -56,6 +83,7 @@ if danger.github.pullRequest.base.ref == "master" {
     }
 }
 
+// MARK: - Updated tests
 // Make sure source changes have their tests updated
 for file in editedFiles where file.hasPrefix("Sources/") {
     let dir = file.dropFirst("Sources/".count).split(separator: "/")[0]
@@ -72,6 +100,7 @@ for file in editedFiles where file.hasPrefix("Sources/") {
     }
 }
 
+//MARK: - Updated test manifests
 // If tests are added, ensure that they're in XCTestManifest / LinuxMain
 if danger.git.createdFiles.contains(where: { $0.hasPrefix("Tests/") }) {
     // LinuxMain doesn't need to change anymore, unless a new XCTestManifests file is added
@@ -100,6 +129,7 @@ if danger.git.createdFiles.contains(where: { $0.hasPrefix("Tests/") }) {
     }
 }
 
+// MARK: - Tasks in PR body
 // Check for incomplete tasks in the PR body
 // Note the difference between the first regex and the later two ("\n" vs "^").
 //   That's the "start of string" character, which I only want to match after I've split
